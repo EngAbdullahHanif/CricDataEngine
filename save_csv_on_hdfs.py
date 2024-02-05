dc14ddd12061a416fb70e704b50cfea7a11a56d1
engAbdullahHanif <engabdullahhanif@gmail.com>
2024-02-05 02:37:06 +0100

    csv udpated

--- save_csv_on_hdfs.py
+++ save_csv_on_hdfs.py
@@ -22,17 +22,36 @@ def save_to_hdfs(data, hdfs_path):
     # Connect to HDFS
     # hdfs_client = InsecureClient('http://<HDFS-NAMENODE-HOST>:<HDFS-NAMENODE-PORT>', user='<HDFS-USERNAME>')
     # hdfs_client = InsecureClient('http://100.80.154.9:9000', user='hanif')
-    hdfs_client = KerberosClient('http://master-node:9870','OPTIONAL')
+    print('Connecting to HDFS')
+    try:
+        hdfs_client = InsecureClient('http://master-node:9870', user='hanif')
+        print('Connected to HDFS')
+    except Exception as e:
+        print('Failed to connect to HDFS')
+        print(e)
+        return
 
-    # Write CSV data to HDFS
-    with hdfs_client.write(hdfs_path, encoding='utf-8') as hdfs_file:
-        hdfs_file.write(csv_data)
+    try:
+        # Write CSV data to HDFS
+        with hdfs_client.write(hdfs_path, encoding='utf-8') as hdfs_file:
+            hdfs_file.write(csv_data)
+        print('Data written to HDFS')
+    except Exception as e:
+        print('Failed to write data to HDFS')
+        print(e)
+
+
 
 data = {'match_name': ['Match1', 'Match2', 'Match3'],
         'venue': ['Venue1', 'Venue2', 'Venue3'],
         'result': ['Win', 'Loss', 'Draw']}
 
+print('Creating DataFrame')
 cricbuzz_data = pd.DataFrame(data)
+print('DataFrame created')
 
-hdfs_path = '/user/hanif/CricDataEngine/Data/cricbuzz_data.csv'  # Example HDFS path, adjust as needed
+print('Saving to HDFS')
+# hdfs_path = '/home/notebook/CricDataEngine/Data/cricbuzz_data.csv' 
+hdfs_path = '/home/hanif/CricDataEngine/data/cricbuzz_data.csv' 
 save_to_hdfs(cricbuzz_data, hdfs_path)
+print('Data saved to HDFS')
