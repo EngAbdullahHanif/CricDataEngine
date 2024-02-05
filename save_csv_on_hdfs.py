e85ff73830534b6ef07164b890b97a918d7ac157
engAbdullahHanif <engabdullahhanif@gmail.com>
2024-02-05 01:15:06 +0100

    hdfs code updated

--- save_csv_on_hdfs.py
+++ save_csv_on_hdfs.py
@@ -1,12 +1,30 @@
+# import pandas as pd
+# from pydoop import hdfs
+
+# def save_to_hdfs(data, hdfs_path):
+#     csv_data = data.to_csv(index=False)
+    
+#     with hdfs.open(hdfs_path, 'wb') as hdfs_file:
+#         hdfs_file.write(csv_data.encode())
+
+# cricbuzz_data = pd.DataFrame(...)  
+# hdfs_path = '/home/hanif/CricDataEngine/Data/cricbuzz_data.csv'  
+# save_to_hdfs(cricbuzz_data, hdfs_path)
+
+
 import pandas as pd
-from pydoop import hdfs
+from hdfs import InsecureClient
 
 def save_to_hdfs(data, hdfs_path):
     csv_data = data.to_csv(index=False)
     
-    with hdfs.open(hdfs_path, 'wb') as hdfs_file:
-        hdfs_file.write(csv_data.encode())
+    # Connect to HDFS
+    hdfs_client = InsecureClient('http://<HDFS-NAMENODE-HOST>:<HDFS-NAMENODE-PORT>', user='<HDFS-USERNAME>')
+
+    # Write CSV data to HDFS
+    with hdfs_client.write(hdfs_path, encoding='utf-8') as hdfs_file:
+        hdfs_file.write(csv_data)
 
 cricbuzz_data = pd.DataFrame(...)  
-hdfs_path = '/user/your_username/cricbuzz_data.csv'  
+hdfs_path = '/user/hanif/CricDataEngine/Data/cricbuzz_data.csv'  # Example HDFS path, adjust as needed
 save_to_hdfs(cricbuzz_data, hdfs_path)
